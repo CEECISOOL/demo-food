0e3b55abc0ffd3bbea1bc7f38e5af8eb5d2cbf75
CEECISOOL <cbritez6@gmail.com>
2022-06-03 00:45:33 -0300

    demo-food

--- api/src/app.js
+++ api/src/app.js
@@ -7,11 +7,13 @@ const routes = require('./routes/index.js');
 require('./db.js');
 
 const server = express();
+const cors = require('cors')
 
 //http://localhost:3000
 
 server.name = 'API';
 
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
